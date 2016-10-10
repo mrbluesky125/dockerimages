9fc617f5d0bc5818aea9f1bdf1e2497ce95555cc
root <zimmermann.emb@googlemail.com>
2016-10-10 17:28:17 +0200

    fix: screensaver removed

--- dlmtesting/start.sh
+++ dlmtesting/start.sh
@@ -8,5 +8,6 @@ sed -i 's/username=ask/username=root/g' /etc/xrdp/xrdp.ini
 sed -i '/\[globals\]/a autorun=sesman-Xvnc' /etc/xrdp/xrdp.ini
 sed -i '/\[globals\]/a hidelogwindow=1' /etc/xrdp/xrdp.ini
 sed -i '/switch_window_key/d' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+rm /etc/xdg/autostart/xscreensaver.desktop
 
 exec /usr/bin/supervisord -n
