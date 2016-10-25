ce2b737e72df7e1245cd0b6c72eda64891b18a02
root <zimmermann.emb@googlemail.com>
2016-10-25 14:44:06 +0200

    add: firefox added to container add: vorlesung share added

--- dlmtesting/start.sh
+++ dlmtesting/start.sh
@@ -4,15 +4,6 @@ PASSWD_ENV_NAME=${CONTAINER}_PASSWD
 PASSWD=$(printenv ${PASSWD_ENV_NAME})
 echo "root:"${PASSWD} | chpasswd
 
-#delete all other xrdp configs
-#sed -i '/\[xrdp2\]/Q' /etc/xrdp/xrdp.ini
-#sed -i '/\[xrdp2\]/d' /etc/xrdp/xrdp.ini
-
-#sed -i 's/username=ask/username=root/g' /etc/xrdp/xrdp.ini
-#sed -i 's/port=-1/port=ask-1/g' /etc/xrdp/xrdp.ini
-#sed -i '/\[globals\]/a autorun=sesman-Xvnc' /etc/xrdp/xrdp.ini
-#sed -i '/\[globals\]/a hidelogwindow=1' /etc/xrdp/xrdp.ini
-
 #sesman config
 sed -i 's/KillDisconnected=0/KillDisconnected=1/g' /etc/xrdp/sesman.ini
 sed -i 's/DisconnectedTimeLimit=0/DisconnectedTimeLimit=10/g' /etc/xrdp/sesman.ini
