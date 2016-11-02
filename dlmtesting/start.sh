31bed00b6ca0096ea1d1fd1df37758e60862049c
root <zimmermann.emb@googlemail.com>
2016-11-02 14:22:31 +0100

    fix: fixed env variables for spyder launcher

--- dlmtesting/start.sh
+++ dlmtesting/start.sh
@@ -12,9 +12,17 @@ sed -i 's/MaxSessions=10/MaxSessions=50/g' /etc/xrdp/sesman.ini
 #fix tab completion
 sed -i '/switch_window_key/d' /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 
+#create spyderlauncher.sh
+touch /root/spyderlauncher.sh
+echo 'export PATH='$PATH >> /root/spyderlauncher.sh
+echo 'export LD_LIBRARY_PATH='$LD_LIBRARY_PATH >> /root/spyderlauncher.sh
+echo 'export PYTHONPATH=/opt/caffe/python:' >> /root/spyderlauncher.sh
+echo '/opt/conda/bin/spyder --new-instance' >> /root/spyderlauncher.sh
+chmod +x /root/spyderlauncher.sh
+
 #create desktop shortcut
 sed -i '/Exec=spyder/d' /root/Desktop/spyder.desktop
-echo "Exec=/opt/conda/bin/spyder --new-instance" >> /root/Desktop/spyder.desktop
+echo "Exec=/root/spyderlauncher.sh" >> /root/Desktop/spyder.desktop
 echo "Icon=/opt/conda/share/pixmaps/spyder.png" >> /root/Desktop/spyder.desktop
 chmod +x /root/Desktop/spyder.desktop
 
