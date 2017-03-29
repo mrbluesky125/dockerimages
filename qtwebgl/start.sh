4b1c437dd783fcef80b90bfe78286b2a41f6f221
Pascal Siegel <siegel.emb@gmail.com>
2017-03-29 08:02:43 +0000

    Update start.sh

--- qtwebgl/start.sh
+++ qtwebgl/start.sh
@@ -2,7 +2,7 @@
 CONTAINER=$CONTAINER
 PASSWD_ENV_NAME=${CONTAINER}_PASSWD
 PASSWD=$(printenv ${PASSWD_ENV_NAME})
-echo "root:"${PASSWD} | chpasswd
+echo "root:root" | chpasswd
 
 #allow root ssh access
 sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
