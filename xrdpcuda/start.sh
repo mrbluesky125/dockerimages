f13f34f0d9372be3eb277ae7475165e8df647f62
Timo Zimmermann <git@timozimmermann.de>
2019-08-29 15:53:28 +0200

    Update start.sh

--- xrdpcuda/start.sh
+++ xrdpcuda/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-CONTAINER=$CONTAINER
-PASSWD_ENV_NAME=${CONTAINER}_PASSWD
-PASSWD=$(printenv ${PASSWD_ENV_NAME})
+PASSWD=$(printenv $PASSWD)
 echo "root:"${PASSWD} | chpasswd
 
 exec /usr/bin/supervisord -n
